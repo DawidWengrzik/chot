ce5e7fcb1699786d703c4337cadb3f4dc85428fa
De Wengrzik <dewengrzik@gmail.com>
2023-01-27 22:34:08 +0100

    initialize project

---
+++ client/src/App/App.js
@@ -0,0 +1,76 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "../Chat/Chat.js";
+import Video from "../Video/Video.js";
+import { BrowserRouter as Router, Switch, Route, Link, Routes, BrowserRouter } from "react-router-dom";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  
+  const [nickname, setNickname] = useState("");
+  const [room, setRoom] = useState("");
+
+  //przetrzymuje wartosc wpisana w polu nickname
+  const NickHandler = (event) => {
+    setNickname(event.target.value);
+  };
+
+  //przetrzymuje wartosc wpisana w polu room
+  const RoomHandler = (event) => {
+    setRoom(event.target.value);
+  };
+
+  //przekazanie danych do serwera, poprzez wartosc "join", a kolejno wypisanie jej w backendzie
+  const JoinRoom = () => {
+    if (nickname !== ("") && room !== ("")) {
+      socket.emit("join", room);
+    } else {
+      alert("Please enter all requirements")
+      window.location.reload();
+    }
+  };
+
+  const JoinVideo = () => {
+    if (nickname !== ("") && room !== ("")) {
+      socket.emit("joinV", room);
+    } else {
+      alert("Please enter all requirements")
+      window.location.reload();
+    }
+  };
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+            <div className="App">
+              <div className='My-form'>
+                <h1>Chat and video application</h1>
+                <h3>Please enter your name:</h3>
+                <input placeholder='Nickname' className="txtInput" onChange={NickHandler}></input>
+                
+                <h3>Please enter room that you want to join:</h3>
+                <input placeholder="Room ID" className="txtInput" onChange={RoomHandler}></input>
+                <div className="buttons-container">
+                  <Link to="/Chat" onClick={JoinRoom}>
+                    <button><span>Let's talk!</span></button>
+                  </Link>
+                  <Link to="/Video" onClick={JoinVideo}>
+                    <button><span>Video chat</span></button>
+                  </Link>
+                </div>                
+              </div>
+            </div>
+        }>
+        </Route>
+        <Route path="/Chat" element={<Chat socket={socket} nickname={nickname} room={room}/>}></Route>
+        <Route path="/Video" element={<Video socket={socket} room={room}/>}></Route>
+      </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/Video/Video.js
@@ -0,0 +1,153 @@
+import './Video.css';
+import React, { useState, useEffect, useRef } from "react";
+import Peer from "simple-peer";
+
+
+
+function Video( { socket, room } ) {
+
+ 	const [ stream, setStream ] = useState()
+
+	const [ receivingCall, setReceivingCall ] = useState(false)
+    const [ caller, setCaller ] = useState()
+	const [ callerSignal, setCallerSignal ] = useState()
+
+	const [ callAccepted, setCallAccepted ] = useState(false)
+	const [ callEnded, setCallEnded] = useState(false)
+	
+    const myVideo = useRef()
+	const userVideo = useRef()
+
+	const connectionRef= useRef()
+
+    const constraints = {
+        'video': true,
+        'audio': true
+    }    
+
+    //wyskakujace okienko pytajace na udostepnienie kamery i mikrofonu oraz ustawienie myVideo na aktualny strumien danych
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia(constraints)
+        .then((stream) => {
+            setStream(stream)
+            myVideo.current.srcObject = stream
+        });
+
+        //proba nawiazania polaczenia od uzytkownika poprzez "callRoom"
+        socket.on("callRoom", (data) => {
+            setReceivingCall(true)
+            setCaller(data.from)
+            setCallerSignal(data.signal)
+        });
+    }, [socket]);
+
+    //nowe polaczenie peer-peer
+	const callRoom = (id) => {
+		const peer = new Peer({
+			initiator: true,
+			trickle: false,
+			stream: stream
+		});
+
+        //przeslanie danych do serwera
+		peer.on("signal", (data) => {
+			socket.emit("callRoom", {
+				roomToCall: room,
+				signalData: data,
+				from: socket.id,
+			});
+		});
+
+        //ustawienie wideo czatu
+		peer.on("stream", (stream) => {
+			userVideo.current.srcObject = stream
+		});
+
+		socket.on("callAccepted", (signal) => {
+			setCallAccepted(true);
+			peer.signal(signal);
+		});
+
+        //obecne polaczenie jest rowne peer'owi
+		connectionRef.current = peer
+	};
+
+    const answerCall = () =>  {
+		setCallAccepted(true)
+		const peer = new Peer({
+			initiator: false,
+			trickle: false,
+			stream: stream
+		});
+
+        //nawiazanie polaczenia wideo
+		peer.on("signal", (data) => {
+			socket.emit("answerCall", { signal: data, to: caller });
+		});
+
+        //ustawienie wideo czatu dla odbiorcy
+		peer.on("stream", (stream) => {
+			userVideo.current.srcObject = stream;
+		});
+
+        //obecne polaczenie jest rowne peer'owi
+		peer.signal(callerSignal)
+		connectionRef.current = peer
+	};
+
+	const leaveCall = () => {
+		setCallEnded(true);
+		connectionRef.current.destroy();
+        window.location.replace("http://localhost:3000");
+	};
+
+    return (
+		<div className="video">
+			<div className='video-container'>
+				<div className={`myvideo ${callAccepted ? "myvideo-accepted" : ''}`}>
+                    <video playsInline muted ref={myVideo} autoPlay/>
+                </div>
+
+                <div className="uservideo">
+                    {callAccepted && !callEnded ?
+                    <video playsInline ref={userVideo} autoPlay  />:
+                    null}
+                </div>
+			</div>
+			
+            <div className="home-user ">
+				<div className="call-info buttons-video">
+					{callAccepted && !callEnded ? (
+						<button onClick={leaveCall}> 
+                            <span>
+								End Call
+							</span>
+                        </button>
+					) : (
+						<button onClick={() => callRoom(room)}>
+                            <span>
+								Voice Chat
+							</span>
+						</button>
+					)}
+				
+				{receivingCall && !callAccepted ? (
+					<div className="caller">
+						<div className='caller-info'>
+							<h1> Someone is calling {console.log(socket)}
+							 </h1>
+							<button onClick={answerCall}>
+								<span>
+									Answer
+								</span>
+							</button>
+						</div>						
+					</div>
+				) : null}
+			    </div>
+			</div>
+        </div>
+	)
+}
+
+export default Video;
