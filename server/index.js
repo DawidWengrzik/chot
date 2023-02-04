7db0a04b16c4fcf0d43e2b1c07f4881a31c9d6ae
De Wengrzik <dewengrzik@gmail.com>
2023-02-04 18:35:38 +0100

    firebase hosting

--- server/index.js
+++ server/index.js
@@ -6,6 +6,28 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 app.use(cors()); // identyfikujemy główny obiekt z mechanizmem cors
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCeTn0ez0tn3CclqV2-Aez671Zg4WjP0kg",
+  authDomain: "chot-559af.firebaseapp.com",
+  projectId: "chot-559af",
+  storageBucket: "chot-559af.appspot.com",
+  messagingSenderId: "917398569742",
+  appId: "1:917398569742:web:56efb8d0f325e73a3d8d5f",
+  measurementId: "G-SSB68P1EZ4"
+};
+
+// Initialize Firebase
+const application = initializeApp(firebaseConfig);
+const analytics = getAnalytics(application);
+
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:3000"],
